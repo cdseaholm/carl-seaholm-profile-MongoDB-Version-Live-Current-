c176bc7c54b33e488a6546e54d2ad0c3a094ef13
cdseaholm <cdseaholm@gmail.com>
2024-03-14 19:45:55 -0600

    3/14/24 1.2

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
         </div>
         <div className='w-2/6 flex-col text-right justify-items-right' style={{paddingLeft: clicked ? 14 : 0}}>
           <h1 className="text-6xl font-bold pt-20">Carl Seaholm</h1>
-          <p className="text-2xl pt-5">Constantly learning, improving, and trying something new. Follow along, check out my Coding projects, current book I'm trying to publish and the other that I'm writing.</p>
+          <p className="text-2xl pt-5">Constantly learning, improving, and trying something new. Follow along, check out my Coding projects, current book I&apos;m trying to publish and the other that I&apos;m writing.</p>
           <p className="text 2xl pt-5 tp-2">Currently focusing on improving my JavaScript and TSX applications. As well as learning to site read while playing piano, improving guitar playing, and writing.</p>
         </div>
       </div>
