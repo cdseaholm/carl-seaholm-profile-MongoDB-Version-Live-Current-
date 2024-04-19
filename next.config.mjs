0510e0de5258e39bc10a86e2e7f39f0e40d1e97e
cdseaholm <cdseaholm@gmail.com>
2024-04-19 09:37:38 -0600

    4/19/24

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
             source: '/api/:path*',
             headers: [
               { key: 'Access-Control-Allow-Credentials', value: 'true' },
-              { key: 'Access-Control-Allow-Origin', value: '*' },
+              { key: 'Access-Control-Allow-Origin', value: 'https://www.carlseaholm.com' },
               {
                 key: 'Access-Control-Allow-Methods',
                 value: 'GET,DELETE,PATCH,POST,PUT',
