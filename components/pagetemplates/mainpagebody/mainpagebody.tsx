9cdfd4b08e8fe52a7ff3e7fe0e731477d82ecb63
cdseaholm <cdseaholm@gmail.com>
2025-04-24 13:53:48 -0600

    4/24/25 - uitest1

--- components/pagetemplates/mainpagebody/mainpagebody.tsx
+++ components/pagetemplates/mainpagebody/mainpagebody.tsx
@@ -1,6 +1,6 @@
 const MainPageBody = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className={`bg-slate-900/50 childFirst w-screen h-screen overflow-hidden`}>
+    <div className={`bg-slate-900/50 childFirst min-h-[500px] w-[100dvw] h-[100dvh] scrollbar-thin scrollbar-webkit`} style={{ overflowX: 'hidden', overflowY: 'auto' }}>
       {children}
     </div>
   );
